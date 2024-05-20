1173bc758e1eb8da85351f288e0d4c1d958daaa1
skipper <skipperuzmaki@gmail.com>
2024-05-21 01:38:52 +0800

    the ultimate version v2

--- app/routers/authors.py
+++ app/routers/authors.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from typing import List
 from app import crud, models, schemas
 from app.dependencies import get_db
+from app.auth.jwt import verify_token
+from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter(
     prefix="/authors",
@@ -10,20 +12,60 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
 
 @router.post("/", response_model=schemas.Author)
-def create_author(author: schemas.AuthorCreate, db: Session = Depends(get_db)):
+def create_author(author: schemas.AuthorCreate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = (
+        db.query(models.User)
+        .filter(models.User.username == token_data.username)
+        .first()
+    )
+    if user is None:
+        raise credentials_exception
     return crud.create_author(db=db, author=author)
 
 
 @router.get("/", response_model=List[schemas.Author])
-def read_authors(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_authors(skip: int = 0, limit: int = 10, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = (
+        db.query(models.User)
+        .filter(models.User.username == token_data.username)
+        .first()
+    )
+    if user is None:
+        raise credentials_exception
     authors = crud.get_authors(db, skip=skip, limit=limit)
     return authors
 
 
 @router.get("/{author_id}", response_model=schemas.Author)
-def read_author(author_id: int, db: Session = Depends(get_db)):
+def read_author(author_id: int, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = (
+        db.query(models.User)
+        .filter(models.User.username == token_data.username)
+        .first()
+    )
+    if user is None:
+        raise credentials_exception
     db_author = crud.get_author(db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -32,8 +74,21 @@ def read_author(author_id: int, db: Session = Depends(get_db)):
 
 @router.put("/{author_id}", response_model=schemas.Author)
 def update_author(
-    author_id: int, author: schemas.AuthorCreate, db: Session = Depends(get_db)
+    author_id: int, author: schemas.AuthorCreate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = (
+        db.query(models.User)
+        .filter(models.User.username == token_data.username)
+        .first()
+    )
+    if user is None:
+        raise credentials_exception
     db_author = crud.get_author(db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -41,7 +96,20 @@ def update_author(
 
 
 @router.delete("/{author_id}", response_model=schemas.Author)
-def delete_author(author_id: int, db: Session = Depends(get_db)):
+def delete_author(author_id: int, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    token_data = verify_token(token, credentials_exception)
+    user = (
+        db.query(models.User)
+        .filter(models.User.username == token_data.username)
+        .first()
+    )
+    if user is None:
+        raise credentials_exception
     db_author = crud.get_author(db, author_id=author_id)
     if db_author is None:
         raise HTTPException(status_code=404, detail="Author not found")
